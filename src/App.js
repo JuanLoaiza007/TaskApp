437bf16ea00e6f4c247f15720478bea2aa510354
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-01-08 14:50:50 -0500

    Update versioning after previous commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const [appState, setAppState] = useState({
     "appName": "TaskApp",
     "showenVersion": "...",
-    "version": "1.2.0",
+    "version": "1.3.0",
     "versionState": "deactivated"
   });
 
